8fab3d96d6a7f48b675cc2f299296250d2ce8d1c
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-09-09 14:28:08 +0700

    fixed errors reported by flake8

--- test_statistics.py
+++ test_statistics.py
@@ -1,23 +1,28 @@
+"""Tests for functions in statistics."""
 from unittest import TestCase
 from statistics import variance, stdev
 from math import sqrt
 
+
 class StatisticsTest(TestCase):
+    """Tests of finding statistics value."""
 
     def test_variance_typical_values(self):
-        """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
-    
+        """Variance of typical values."""
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
+
     def test_variance_non_integers(self):
-        """variance should work with decimal values"""
+        """Variance should work with decimal values."""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
         self.assertAlmostEqual(4.0, variance([0.1, 4.1]), delta=0.001)
         # variance([0,4,4,8]) == 8
-        self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]), delta=0.001)
+        self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]),
+                               delta=0.001)
 
     def test_stdev(self):
+        """Standard devaiation of values."""
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
         # simple test
@@ -29,4 +34,3 @@ class StatisticsTest(TestCase):
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
