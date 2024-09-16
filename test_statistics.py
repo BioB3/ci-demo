4f2c7813443a1e8294d8c6944bf036e688524f9d
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-09-16 20:51:07 +0700

    added tests to increase coverage and added .coveragerc

--- test_statistics.py
+++ test_statistics.py
@@ -1,17 +1,30 @@
 """Tests for functions in statistics."""
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import average, variance, stdev
 from math import sqrt
 
 
-class StatisticsTest(TestCase):
-    """Tests of finding statistics value."""
+class AverageTest(TestCase):
+    """Tests for average function."""
+
+    def test_average_interger(self):
+        """Average of integers."""
+        self.assertEqual(10, average([10, 10, 10, 10, 10]))
+        self.assertEqual(3, average([1, 2, 3, 4, 5]))
+        self.assertEqual(6, average([10, 2, 8, 4, 6]))
+
+    def test_average_none(self):
+        """Average of empty list raise ValueError."""
+        self.assertRaises(ValueError, average, [])
+
+
+class VarianceTest(TestCase):
+    """Tests for variance function."""
 
     def test_variance_typical_values(self):
         """Variance of typical values."""
         self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
         self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
-        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_non_integers(self):
         """Variance should work with decimal values."""
@@ -21,6 +34,14 @@ class StatisticsTest(TestCase):
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]),
                                delta=0.001)
 
+    def test_variance_none(self):
+        """Variance of empty list raise ValueError."""
+        self.assertRaises(ValueError, variance, [])
+
+
+class StdevTest(TestCase):
+    """Tests for stdev function."""
+
     def test_stdev(self):
         """Standard devaiation of values."""
         # standard deviation of a single value should be zero
@@ -29,8 +50,3 @@ class StatisticsTest(TestCase):
         self.assertEqual(2.0, stdev([1, 5]))
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
-
-
-if __name__ == '__main__':
-    import unittest
-    unittest.main(verbosity=1)
